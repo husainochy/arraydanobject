648176c263f9a77439452519742d701641dec305
Husain <ochy22@gmail.com>
2021-10-12 02:01:43 +0800

    no message

---
+++ array.php
@@ -0,0 +1,5 @@
+<?php
+
+$array = ["TV","HP","Kipas Anging"];
+
+echo '<pre>', print_r($array), '</pre>';
---
+++ arraykeobject.php
@@ -0,0 +1,8 @@
+<?php
+
+$array = ["TV","HP","Kipas Anging"];
+
+$object = json_decode(json_encode($array));
+var_dump($object);
+
+echo '<pre>', print_r($array), '</pre>';
