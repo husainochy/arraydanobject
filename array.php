a1a014a0db95e3fc5358cc217b796b08576306c8
Husain <ochy22@gmail.com>
2021-10-13 16:20:50 +0800

    no message

--- array.php
+++ array.php
@@ -1,5 +1,32 @@
 <?php
 
-$array = ["TV","HP","Kipas Anging"];
+$pakaian = (object)[
+	'nama' => 'kameja' ,
+	'ukuran' => 'L' ,
+	'harga' => 100000,
+	'warna' => [
+		'polos' => 'putih' ,
+		'merek' => 'novear' ,
+			]
+	
+];
 
-echo '<pre>', print_r($array), '</pre>';
+$perabotan = (object)[
+	'nama' => 'ricecooker' ,
+	'ukuran' => '2 kilo' ,
+	'harga' => 250000,
+	'warna' => [
+		'polos' => 'putih' ,
+		'merek' => 'maspion' ,
+			]
+	
+];
+// cara mengetahui object multidimensi
+echo "<pre>";
+echo "<pre>";
+print_r($pakaian);
+print_r($perabotan);
+echo "<pre>";
+echo "<pre>";
+
+?>
---
+++ arraydanobject.php
@@ -0,0 +1,65 @@
+<?php 
+
+// Array Online Course
+$siswaArr = array(
+    'nama_sekolah' => 'SDN 16 Mataram',
+    'lama_sekolah' => '2 jam',
+    'Kelas' => 'III',
+    'murid' => array(
+        array(
+            'nama' => 'husain',
+            'umur' => '8 tahun',
+            'trackers' => array(
+                'masuk' => '10 juni 2019',
+                'progress' => 50
+            )
+        ),
+        array(
+            'nama' => 'wira',
+            'umur' => '8 tahun',
+            'trackers' => array(
+                'masuk' => '10 juni 2019',
+                'progress' => 40
+            )
+        ),
+    )
+);
+
+// print_r($courseArr);
+
+// Object Online Course
+$siswaObj = (object)[
+    'nama_sekolah' => 'SDN 16 Mataram',
+    'lama_sekolah' => '2 jam',
+    'Kelas' => 'III',
+    'murid' => array(
+        (object)[
+            'nama' => 'husain',
+            'umur' => 8,
+            'trackers' => (object)[
+                'masuk' => '10 juni 2019',
+                'semester' => 1
+            ]
+        ],
+        (object)[
+            'nama' => 'wira',
+            'umur' => 8,
+            's' => (object)[
+                'masuk' => '10 juni 2019',
+                'semester' => 1
+            ]
+        ],
+    )
+];
+
+echo '<pre>';
+print_r($siswaObj);
+echo '</pre>';
+
+echo '<br>';
+
+echo "siswa : {$siswaObj->nama_sekolah} <br>" ;
+echo "nama_siswa : {$siswaObj->murid[0]->nama} <br>" ;
+echo "semester : {$siswaObj->murid[0]->trackers->semester} <br>" ;
+
+?>
